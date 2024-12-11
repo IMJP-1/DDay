5da6f3c06d931746c2965a9eeedf8d4dac47dc49
Jaimin <jhp02016@gmail.com>
2024-12-11 17:28:15 +0530

    config changed back

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 // module.exports = {
 //   output: 'export',
 //   basePath: '/DDay',
 //   assetPrefix: '/DDay',
 // };
+export default nextConfig;
 
-// export default nextConfig;
-
-export default {
-  output: 'export',
-  basePath: '/DDay',
-  assetPrefix: '/DDay',
-};
+// export default {
+//   output: 'export',
+//   basePath: '/DDay',
+//   assetPrefix: '/DDay',
+// };
 
